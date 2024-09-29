c6d740395de58041b5e78f40c872823ae2c00e68
rishueee <jhapinki567@gmail.com>
2024-09-29 21:33:39 +0530

    Static Member Function Implemented

--- HustleNFlow.java
+++ HustleNFlow.java
@@ -7,15 +7,15 @@ class Artist {
 
     private static int totalArtists = 0;
     private static double totalMoneyEarned = 0;
-    private static int totalFans = 0; 
+    private static int totalFans = 0;
 
     public Artist(String name, int fanBase, double money) {
         this.name = name;
         this.fanBase = fanBase;
         this.money = money;
-        totalArtists++; 
-        totalMoneyEarned += money; 
-        totalFans += fanBase; 
+        totalArtists++;
+        totalMoneyEarned += money;
+        totalFans += fanBase;
     }
 
     public void releaseSongs(Song[] songs) {
@@ -24,8 +24,8 @@ class Artist {
             int fansGained = popularity * 10;
             fanBase += fansGained;
             money += popularity * 50;
-            totalMoneyEarned += popularity * 50; 
-            totalFans += fansGained; 
+            totalMoneyEarned += popularity * 50;
+            totalFans += fansGained;
             System.out.println(name + " released '" + song.getTitle() + "' and gained " + fansGained + " fans!");
         }
     }
@@ -34,8 +34,8 @@ class Artist {
         int concertFans = 50;
         fanBase += concertFans;
         money += 1000;
-        totalMoneyEarned += 1000; 
-        totalFans += concertFans; 
+        totalMoneyEarned += 1000;
+        totalFans += concertFans;
         System.out.println(name + " performed at a concert and gained " + concertFans + " fans!");
     }
 
@@ -49,6 +49,14 @@ class Artist {
 
     public double getMoney() {
         return money;
+    }
+
+    
+    public static void displayTotalStats() {
+        System.out.println("\n--- Total Stats ---");
+        System.out.println("Total Artists: " + totalArtists);
+        System.out.println("Total Money Earned by All Artists: $" + totalMoneyEarned);
+        System.out.println("Total Fans Gained by All Artists: " + totalFans);
     }
 
     public static int getTotalArtists() {
@@ -142,9 +150,8 @@ class Main {
             System.out.println("Name: " + artists[j].getName() + ", Fan Base: " + artists[j].getFanBase() + ", Money: $" + artists[j].getMoney());
         }
 
-        System.out.println("\nTotal Artists: " + Artist.getTotalArtists());
-        System.out.println("Total Money Earned by All Artists: $" + Artist.getTotalMoneyEarned());
-        System.out.println("Total Fans Gained by All Artists: " + Artist.getTotalFans());
+        
+        Artist.displayTotalStats(); 
 
         scanner.close();
     }
